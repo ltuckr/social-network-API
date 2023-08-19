eae7962aafef761504e8d4de064fe8e3faf47dba
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-08-19 09:11:58 -0400

    user schema completed

--- Develop/models/thought.js
+++ Develop/models/thought.js
@@ -1,27 +1,2 @@
 const {schema, model, Types} = require('mongoose');
 
-const UserSchema = new schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: 'Username is required',
-            trim: true
-        },
-
-        email: {
-            type: String,
-            unique: true,
-            required: 'Email is required',
-            match: [/.+@.+\..+/]
-        },
-
-        thoughts: [
-            {
-                type: Types.ObjectId,
-                ref: 'Thought'
-            }
-        ],
-
-        friends: [
-            {
